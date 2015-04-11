df0102253415a4363e88ffffc4fd71eea507833f
Derrick Lord <derrick.lord@gmail.com>
2015-04-10 14:55:22 -1000

    Added Delete Feature

--- public/js/app.js
+++ public/js/app.js
@@ -14,18 +14,31 @@ app.config(['$routeProvider', function($routeProvider){
 }]);
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http){
-	$http.get('http://localhost:3000/api/books').
-			success(function(data){
-				$scope.authors = data.authors;
-				$scope.books = data.books;
-				$scope.data = data;
-			}).error(function(error){
-				console.log(error);
-	});
+
+	$scope.getData = function(){
+		$http.get('http://localhost:3000/api/books').
+				success(function(data){
+					$scope.authors = data.authors;
+					$scope.books = data.books;
+					$scope.data = data;
+				}).error(function(error){
+					console.log(error);
+		});		
+	}
+
+
+	$scope.delete = function(id){
+		//alert('Book Deleted: ' + id);
+		$http.delete('http://localhost:3000/api/books/'+id);
+		$scope.getData();
+
+	}
+
+	$scope.getData();
 
 }]);
 
-app.controller('NewController', ['$scope','$http', function($scope, $http){
+app.controller('NewController', ['$scope','$http', '$location', function($scope, $http, $location){
 	$scope.book = {
 		isbn: '',
 		title: '',
@@ -46,6 +59,8 @@ app.controller('NewController', ['$scope','$http', function($scope, $http){
 						img: 'img/book2.jpg',
 						year: 0
 					}
+
+					$location.path('/');
 				}).error(function(error){
 					console.log(error);
 		});		
