26535411e8b87dfb9b7672341ca1f9b8df0ec4be
Derrick Lord <derrick.lord@gmail.com>
2015-04-10 14:10:15 -1000

    Added http POST for new Books

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,53 @@
-var app = angular.module('app', ['ngMaterial']);
+var app = angular.module('app', ['ngMaterial', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/new', {
+			templateUrl: 'views/new.html',
+			controller: 'NewController'
+		})
+		.otherwise({
+			templateUrl: 'views/home.html',
+			controller: 'MainController',
+			controllerAs: 'main'
+		})
+}]);
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http){
 	$http.get('http://localhost:3000/api/books').
 			success(function(data){
 				$scope.authors = data.authors;
 				$scope.books = data.books;
+				$scope.data = data;
 			}).error(function(error){
 				console.log(error);
 	});
 
+}]);
+
+app.controller('NewController', ['$scope','$http', function($scope, $http){
+	$scope.book = {
+		isbn: '',
+		title: '',
+		author: '',
+		genre: '',
+		img: 'img/book2.jpg',
+		year: 0
+	}
+
+	$scope.create = function(){
+		$http.post('http://localhost:3000/api/books', $scope.book).
+				success(function(data){
+					$scope.book = {
+						isbn: '',
+						title: '',
+						author: '',
+						genre: '',
+						img: 'img/book2.jpg',
+						year: 0
+					}
+				}).error(function(error){
+					console.log(error);
+		});		
+	}
 }]);
